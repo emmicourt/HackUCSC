8804828fadffae282a37398757a4fc303b24e740
Emily Bettencourt <EmmyB@dhcp-169-233-185-223.cruznet.ucsc.edu>
2016-01-30 16:46:17 -0800

    addtion of mcdonalds database

--- HackUCSC/App.java
+++ HackUCSC/App.java
@@ -2,12 +2,12 @@
 // main class 
 //-----------------------------------------------------------------------------
 import java.util.*;
-
+import java.util.Scanner;
 
 public class App
 {
 	// variables 
-	public ArrayList<Resturant> database = new ArrayList<Resturant>();
+	public static ArrayList<Resturant> database = new ArrayList<Resturant>();
 
 	//Methods-------------------------------------------------------------
 	public void addResturant(Resturant a)
@@ -34,20 +34,21 @@ public class App
 		return contains; 
 	}
 
-	public Resturant lowestCal(String menuType)
+
+	public static Resturant lowestCal(String menuType)
 	{
-		double minCal = 0;
-		double minCalRestIndex = 0;
 		int i;
 
 		for(i=0; i < database.size(); i++)
 		{
 			Resturant a = database.get(i);
+			double minCalRestIndex = 0;
 			if(a.hasMenuType(menuType))
 			{
-				for(int j = 0; i < a.menu.size(); i++)
+				for(int j = 0; j < a.menu.size(); j++)
 				{
 					MenuItems b = a.menu.get(i);
+					double minCal = b.getCalories();
 					if(b.getMenuType().equals(menuType))
 					{
 						double menuCals = b.getCalories();
@@ -63,14 +64,71 @@ public class App
 		return database.get(i);
 	}
 
-	public void main(String[] args)
+	public static boolean checkPrecence(String type)
 	{
+		boolean x = false;
+		for(int i = 0; i < database.size(); i++)
+		{
+			if(database.get(i).hasMenuType(type))
+			{
+				x = true;
+			}
+		}
+		return x;
+	}
+
+	public static void main(String[] args)
+	{
+		String input;
+		Resturant healthiest;
+
+		Scanner sc = new Scanner(System.in);
+
 
 		Resturant mcd = new Resturant("McDonalds");
 		database.add(mcd);
-		MenuItems burger = new MenuItems("burger",500);
-		mcd.menu.add(burger);
+		MenuItems mcdburger1 = new MenuItems("Quarter Pounder with Cheese",540);
+		mcd.menu.add(mcdburger1);
+		MenuItems mcdburger2 = new MenuItems("Double Quarter Pounder with Cheese",780);
+		mcd.menu.add(mcdburger2);
+		MenuItems mcdburger3 = new MenuItems("Hamburger",250);
+		mcd.menu.add(mcdburger3);
+		MenuItems mcdburger4 = new MenuItems("Cheeseburger",300);
+		mcd.menu.add(mcdburger4);
+		MenuItems mcdburger5 = new MenuItems("BBQ Ranch Burger",350);
+		mcd.menu.add(mcdburger5);
+		MenuItems mcdburger6 = new MenuItems("Grilled Onion Cheddar",310);
+		mcd.menu.add(mcdburger6);
+		MenuItems mcdburger7 = new MenuItems("Double Cheeseburger",440);
+		mcd.menu.add(mcdburger7);
+		MenuItems mcdburger8 = new MenuItems("McDouble",390);
+		mcd.menu.add(mcdburger8);
+		MenuItems mcdburger9 = new MenuItems("Bacon McDouble",460);
+		mcd.menu.add(mcdburger9);
+		MenuItems mcdburger10 = new MenuItems("Daily Double",440);
+		mcd.menu.add(mcdburger10);
+		MenuItems mcdburger11 = new MenuItems("Jalapeño Double",440);
+		mcd.menu.add(mcdburger11);
+		MenuItems mcdburger12 = new MenuItems("Quarter Pounder Deluxe",600);
+		mcd.menu.add(mcdburger12);
 
+		while(true)
+		{
+			System.out.println("Enter what you would like to eat: ");
+			input = sc.next();
+
+		
+			if(checkPrecence(input))
+			{
+				//healthiest = lowestCal(input);
+				break;
+			}
+			else
+			{
+				System.out.println("Option not available");
+			}
+		}
+	
 	}
 
 }
